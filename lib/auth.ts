a4a9142e9e191ca6f6d4be3340c0ec75b6373ff3
NZYM <enzsama.dev@gmail.com>
2025-10-04 14:39:53 +0100

    Add email verification and password reset with resend, add google social login to auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,9 @@
+import sendMail from "@/actions/email";
+import EmailVerification from "@/components/email/EmailVerification";
+import PasswordReset from "@/components/email/PasswordReset";
 import { db } from "@/db/drizzle";
 import { account, session, user, verification } from "@/db/schema/auth-schema";
+import { render } from "@react-email/render";
 import { betterAuth, type BetterAuthOptions } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
@@ -12,6 +16,39 @@ export const auth = betterAuth({
     cookieCache: {
       enabled: true,
       maxAge: 5 * 60, // CACHE DURATION IN SECONDS
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 32,
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url }) => {
+      await sendMail({
+        to: user.email,
+        subject: "Reset your password",
+        template: await render(PasswordReset(url)),
+      });
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      const verificationUrl = url + "email-verified";
+      await sendMail({
+        to: user.email,
+        subject: "Verify your email address",
+        template: await render(EmailVerification(verificationUrl)),
+      });
+    },
+  },
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
 } satisfies BetterAuthOptions);
