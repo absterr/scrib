29b7c741199c25d1e66b57c07e87f5f99d7837af
NZYM <enzsama.dev@gmail.com>
2025-10-05 13:14:59 +0100

    Add app home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,33 @@
-const Homepage = () => <div>Hello world!</div>;
+import { auth } from "@/lib/auth";
+import { getUserRecentNotes } from "@/lib/queries";
+import { getFirstName } from "@/lib/utils";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default Homepage;
+const HomePage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session || !session.user) redirect("/login");
+
+  const userInfo = {
+    id: session.user.id,
+    name: session.user.name,
+  };
+  const userRecentNotes = await getUserRecentNotes(userInfo.id);
+
+  return (
+    <section className="max-w-4xl py-8 mx-auto px-4">
+      <header className="mb-12 flex justify-center">
+        <h1>Hello, {getFirstName(userInfo.name)}</h1>
+      </header>
+
+      <div>
+        <h2 className="font-light text-lg mb-4">Recent notes</h2>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,9 @@ export type UserRole = "owner" | "admin" | "editor";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getFirstName = (fullname: string) => {
+  const names = fullname.trim().split(/\s+/)[0];
+  const firstName = names.charAt(0).toUpperCase() + names.slice(1);
+  return firstName;
+};
