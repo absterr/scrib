6e1af342b4f592e96ce225eadcf0d3c3334ff6d5
NZYM <enzsama.dev@gmail.com>
2025-10-07 15:33:08 +0100

    Add 'home' route group and layout for home page, add navbar component

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,36 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import AppSidebar from "../_AppSidebar";
+import CustomTrigger from "../CustomTrigger";
+import Navbar from "../Navbar";
+
+export default async function AppLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session)
+    return (
+      <div className="w-full h-screen grid grid-rows-[auto, 1fr]">
+        <div className="flex justify-center py-6">
+          <Navbar />
+        </div>
+        <main className="overflow-y-auto scroll-smooth flex-1">{children}</main>
+      </div>
+    );
+
+  return (
+    <div className="flex">
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="flex-1">
+          <CustomTrigger />
+          <main>{children}</main>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
+}
---
+++ app/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useSession } from "@/lib/auth-client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const Navbar = () => {
+  const { data, isPending } = useSession();
+  const pathname = usePathname();
+
+  // TODO: ADD THE THEME SWITCH WHERE 'null' IS
+
+  return (
+    <nav className="max-w-xl w-full h-fit bg-neutral-200 rounded-4xl flex justify-between items-center px-4 py-2 gap-8 min-h-14">
+      <Link href={"/"}>
+        <span className="italic font-bold text-lg px-1">Scrib</span>
+      </Link>
+      <div className="flex gap-5 items-center">
+        {pathname !== "/pricing" && (
+          <Link href={"/pricing"} className="font-semibold hover:underline">
+            Pricing
+          </Link>
+        )}
+        {!isPending && (!data || !data.user) ? (
+          <>
+            <Link href={"/login"} className="font-semibold hover:underline">
+              Log in
+            </Link>
+            <Link href={"/signup"}>
+              <Button className="rounded-3xl text-base py-6 border hover:cursor-pointer">
+                Join for free
+              </Button>
+            </Link>
+          </>
+        ) : null}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
