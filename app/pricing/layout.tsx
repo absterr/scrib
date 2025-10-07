d77f6fb822e686c4aab8398c0780e5f38d74d307
NZYM <enzsama.dev@gmail.com>
2025-10-07 16:02:27 +0100

    Add pricing page layout (I forgot lol)

---
+++ app/pricing/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../Navbar";
+
+export default function LeadLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-full h-screen grid grid-rows-[auto, 1fr]">
+      <div className="flex justify-center py-6">
+        <Navbar />
+      </div>
+      <main className="overflow-y-auto scroll-smooth flex-1">{children}</main>
+    </div>
+  );
+}
