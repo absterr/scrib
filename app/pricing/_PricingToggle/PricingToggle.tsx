fa402bcb7de1b8473cc68af5ac85fef5e012ff1b
NZYM <enzsama.dev@gmail.com>
2025-10-07 16:23:14 +0100

    Add pricing toggle component

---
+++ app/pricing/_PricingToggle/PricingToggle.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import PricingCard from "./PricingCard";
+import PricingTable from "./PricingTable";
+import { cardDetails, details, tableSections } from "../details";
+
+const PricingToggle = () => {
+  const [bill, setBill] = useState<"Monthly" | "Yearly">("Monthly");
+  const options: (typeof bill)[] = ["Monthly", "Yearly"];
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <fieldset
+        role="radiogroup"
+        aria-label="Billing cadence"
+        className="p-1.5 rounded-3xl flex gap-2 bg-gray-200 text-sm font-semibold"
+      >
+        {options.map((opt) => (
+          <label
+            key={opt}
+            className={cn(
+              "rounded-3xl py-2 px-3 cursor-pointer transition-colors duration-300",
+              bill === opt ? "bg-background text-foreground" : "text-gray-400"
+            )}
+          >
+            <input
+              type="radio"
+              name="billing"
+              value={opt}
+              checked={bill === opt}
+              onChange={() => setBill(opt)}
+              className="sr-only"
+            />
+            {opt}
+          </label>
+        ))}
+      </fieldset>
+
+      <p className="pb-6 font-semibold text-neutral-400">
+        <span className="text-neutral-700">Save 16%</span> on a yearly
+        subscription
+      </p>
+
+      <div className="flex gap-6 pb-24">
+        {cardDetails.map((detail) => (
+          <PricingCard key={detail.name} cardDetail={detail} bill={bill} />
+        ))}
+      </div>
+
+      <div className="py-36">
+        <PricingTable
+          details={details}
+          bill={bill}
+          tableSections={tableSections}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PricingToggle;
